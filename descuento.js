d30f49bcfe2c0b2d72ca6edbbf8176a50d06032b
Lucas Veron Vazquez <lucasveronvazquez@gmail.com>
2022-08-02 14:56:51 -0300

    bebibas

--- descuento.js
+++ descuento.js
@@ -1,16 +1,43 @@
-function calcularDescuento(precio, desc){
-  
-
-    let precioConDesc= precio-(precio * desc)/100
-    alert("el precio con descuento es de $"+precioConDesc)
-    precio = parseFloat(prompt("ingrese el precio del producto"))
-    desc = parseFloat(prompt("ingrese el descuento %"))
-    calcularDescuento(precio, desc)
-
+class bebida {
+    constructor(nombre, tipo, precio, alcohol, stock){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.precio = precio;
+        this.alcohol = alcohol;
+        this.stock = stock;
+    }
 }
 
-let precio = parseFloat(prompt("ingrese el precio del producto"))
-let desc = parseFloat(prompt("ingrese el descuento %"))
+const bebidas = []
 
-calcularDescuento(precio, desc)
+bebidas.push(new bebida("PEPSI", "GASEOSA", "$299", false, true))
+bebidas.push(new bebida("COCA-COLA", "GASEOSA", "$320", false, true ))
+bebidas.push(new bebida("FANTA", "GASEOSA", "$250", false, false))
+bebidas.push(new bebida("PATAGONIA", "CERVEZA","$499", true, true))
+bebidas.push(new bebida("ANDES", "CERVEZA", "$370", true, false))
+bebidas.push(new bebida("QUILMES", "CERVEZA", "$350", true, true))
+bebidas.push(new bebida("ELEMENTO", "VINO", "$400" , true, true))
+bebidas.push(new bebida("RUTINI", "VINO", "$799", true, false))
+bebidas.push(new bebida("JACK DANIELS", "WHISKY", "$1200", true, true))
+bebidas.push(new bebida("HAVANA", "RON", "$899", true, true ))
 
+
+console.table(bebidas)
+
+function buscarBebidas(bebida, nombre){
+  
+    const bebidaEncontrada = bebidas.find((beb)=>beb.nombre == nombre);
+    return bebidaEncontrada; 
+}
+
+for (let i=1;i<=20;i++){
+    let bebidaBuscada=prompt("Ingresa el nombre de la bebida").toUpperCase();
+    const encontrada=buscarBebidas(bebida,bebidaBuscada);
+    if(encontrada != undefined){
+        console.log(encontrada);
+    }else{
+        console.log("Bebida no disponible");
+    }
+    
+}
+ 
--- iva.js
+++
@@ -1,8 +0,0 @@
-function calcularIva (precio){
-    let precioConIva=precio * 1.21
-    alert ("el precio con IVA inclcuido es $" + precioConIva )
-    calcularIva(parseFloat(prompt("Ingrese el precio")));
-}
-
-let precio = parseFloat (prompt ("ingrese el precio"))
-calcularIva (precio)
